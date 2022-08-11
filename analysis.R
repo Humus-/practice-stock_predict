1bbfef1fc323364f53e0594e22e313f729e1b4ac
Himanshu <Humus->
2022-08-11 06:37:32 -0400

    changes before reinstall

--- analysis.R
+++ analysis.R
@@ -1,4 +1,18 @@
+library(tidyr)
+library(ggplot2)
+
+# LSTM
+# Arima
+# random forest
+# regression
+# fourier series
+
+if (Sys.getenv("USERNAME") == 'humus') {
+  setwd('workspace/Projects/practice-stock_predict')
+}
+
 fundamentals = read.csv('./fundamentals.csv')
 prices = read.csv('prices.csv')
 securities = read.csv('securities.csv')
 prices_split_adjusted = read.csv('prices-split-adjusted.csv')
+tsla_data = read.csv('HistoricalData_TSLA.csv')
